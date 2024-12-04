3cc9f3009dd754996987aacb45d22e2837296771
Charles Adu Nkansah <charles.nkansah@amalitech.com>
2024-12-04 16:39:30 +0000

    update dag

--- dags/weather_data_pipeline.py
+++ dags/weather_data_pipeline.py
@@ -6,7 +6,8 @@ from airflow.providers.postgres.operators.postgres import PostgresOperator
 from datetime import datetime, timedelta
 import json
 
-# Default args for the DAG
+
+
 default_args = {
     'owner': 'Charles',
     'email': ['charles.nkansah@amalitech.com'],
@@ -16,13 +17,15 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-# Task 3: Transform the data and generate SQL
+
+
 def transform_weather_data(**context):
     # Retrieve raw data from XCom
     raw_data = context['ti'].xcom_pull(task_ids='fetch_weather_data')
     data = json.loads(raw_data)
 
-    # Perform transformations
+
+
     city = data['name']
     temp_kelvin = data['main']['temp']
     temperature_fahrenheit = (temp_kelvin - 273.15) * 9 / 5 + 32
@@ -30,16 +33,17 @@ def transform_weather_data(**context):
     humidity = data['main']['humidity']
     timestamp = datetime.utcfromtimestamp(data['dt']).isoformat()
 
-    # Generate the SQL query
+
     sql = f"""
         INSERT INTO daily_weather (temp_fahrenheit, pressure, humidity, timestamp, city)
         VALUES ({temperature_fahrenheit}, {pressure}, {humidity}, '{timestamp}', '{city}');
     """
 
-    # Push SQL to XCom
+
     context['ti'].xcom_push(key='weather_sql', value=sql)
 
-# DAG definition
+
+
 with DAG(
     dag_id='weather_data_pipeline',
     default_args=default_args,
@@ -49,7 +53,8 @@ with DAG(
     catchup=False,
 ) as dag:
 
-    # Task 1: Check if the API is online
+
+
     api_check = HttpSensor(
         task_id='check_api',
         http_conn_id='weather_api',
@@ -58,7 +63,8 @@ with DAG(
         poke_interval=5,
     )
 
-    # Task 2: Fetch the weather data
+
+
     fetch_weather_data = SimpleHttpOperator(
         task_id='fetch_weather_data',
         http_conn_id='weather_api',
@@ -68,19 +74,19 @@ with DAG(
         log_response=True,
     )
 
-    # Task 3: Transform data and generate SQL
+
     transform_data = PythonOperator(
         task_id='transform_weather_data',
         python_callable=transform_weather_data,
         provide_context=True,
     )
 
-    # Task 4: Load data into PostgreSQL
+
     load_data = PostgresOperator(
         task_id='load_weather_data',
         postgres_conn_id='postgres_local',
         sql="{{ ti.xcom_pull(task_ids='transform_weather_data', key='weather_sql') }}",
     )
 
-    # Task dependencies
+   
     api_check >> fetch_weather_data >> transform_data >> load_data
